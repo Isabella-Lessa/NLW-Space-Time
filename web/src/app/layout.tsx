192f31590195ec1f963da2d81210ffb7311f0b4d
Isabella <isabella.lessa27@gmail.com>
2023-05-21 14:57:27 -0300

    feat: created mobile and web projects

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+// Next Font evita o comportamento de delay até carregar a fonte correta como geralmente acontece na maneira tradicional de importação de fontes, como @font...
+// layout é o bady da aplicação
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+const baijamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW Spacetime',
+  description:
+    'Uma cápsula do tempo com React, Next.js, TailwindCSS e TypeScript',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baijamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
